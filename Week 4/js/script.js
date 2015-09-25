f2a28504655b1768ec892dd831f162c03a11b55d
AuntieEm <Emily.davis.5828@gmail.com>
2015-09-24 21:42:48 -0500

    coding

--- Week 4/js/script.js
+++ Week 4/js/script.js
@@ -1,3 +1,7 @@
-/**
- * Created by Alys1 on 9/23/15.
- */
+function sendEmail() {
+    			var str="name:  " + document.forms.request-form.firstName.value + "\n" + "email:  " + document.forms.request-form.email.value + "\n" + "phone:  " + document.forms.request-form.phone.value;
+    			var link = document.getElemetByID("mylink");
+    			link.href="/mailto:emilya@auntieemsinc.com?subject=form price&estimage&request=" + str;
+    			clickLink(link);
+    				return false;
+    		}
